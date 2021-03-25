26500bdb719f68fd50f5138da7173dc6fe819abb
PiereNewcomer <mandora2004@gmail.com>
2021-03-25 08:58:53 +0700

    first commit

---
+++ client/src/components/views/BlogPage/Sections/CreateBlogPage.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+
+import { Typography, Button, Form, message } from "antd";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import QuillEditor from "../../../editor/QuillEditor";
+
+const { Title } = Typography;
+
+function CreateBlogPage(props) {
+  const user = useSelector((state) => state.user);
+
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState([]);
+
+  const onEditorChange = (value) => {
+    setContent(value);
+    console.log(content);
+  };
+
+  const onFilesChange = (files) => {
+    setFiles(files);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    setContent("");
+
+    if (user.userData && !user.userData.isAuth) {
+      return alert("Please Log in first");
+    }
+
+    const variables = {
+      content: content,
+      userID: user.userData._id,
+    };
+
+    axios.post("/api/blog/createPost", variables).then((response) => {
+      console.log(response);
+      if (response) {
+        message.success("Post Created!");
+
+        setTimeout(() => {
+          props.history.push("/blog");
+        }, 2000);
+      }
+    });
+  };
+
+  return (
+    <div>
+      <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+        <div style={{ textAlign: "center" }}>
+          <Title level={2}> Editor</Title>
+        </div>
+        <QuillEditor
+          placeholder={"Start Posting Something"}
+          onEditorChange={onEditorChange}
+          onFilesChange={onFilesChange}
+        />
+
+        <Form onSubmit={onSubmit}>
+          <div style={{ textAlign: "center", margin: "2rem" }}>
+            <Button
+              size="large"
+              htmlType="submit"
+              className=""
+              onSubmit={onSubmit}
+            >
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default CreateBlogPage;
